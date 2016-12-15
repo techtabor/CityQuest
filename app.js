8d28cc7c95bae0bfb9044a8d946fb3c2f2f16a92
Marcell Szakály <marci07iq@outlook.com>
2016-12-15 14:59:48 +0100

    New backend 3

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var db = require('./db.js');
 var http = require('http');
 var httpdispatcher = require('httpdispatcher');
@@ -111,65 +110,4 @@ dispatcher.onPost("/Test", function(req, res) {
 	res.write("asdfghj");
 	res.end();
 });
-=======
-var db = require('./db.js');
-var dispatcher = require('httpdispatcher');
 
-
-function cb(err, res) {
-	console.log(res);
-}
-/*
-//fooldal
-app.get('/', function (req, res) {
-    console.log('request homera');
-    db.query("SELECT * FROM Asd WHERE Test<=(?)", cb, [[5],[8],[48]]);
-	res.end();
-});
-
-
-app.listen(2017, function () {
-    console.log('Elindult a szerver');
-});*/
-
-var http = require('http');
-
-//Lets define a port we want to listen to
-const PORT=2017; 
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
-
-//Lets use our dispatcher
-function handleRequest(request, response){
-    try {
-        //log the request on console
-        console.log(request.url);
-        //Disptach
-        dispatcher.dispatch(request, response);
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-//For all your static (js/css/images/etc.) set the directory name (relative path).
-//dispatcher.setStatic('resources');
-
-//A sample GET request    
-dispatcher.onGet("/page1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Page One');
-});    
-
-//A sample POST request
-dispatcher.onPost("/post1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Got Post Data');
-});
->>>>>>> 75ec937c19886ec993b766c7e99c5af64da08826
--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var fs = require("fs");
 var file = "test.db";
 var exists = fs.existsSync(file);
@@ -31,34 +30,3 @@ module.exports = {
 		db.close();
 	}
 }
-=======
-var fs = require("fs");
-var file = "test.db";
-var exists = fs.existsSync(file);
-var sqlite3 = require("sqlite3").verbose();
-
-module.exports = {
-	query: function(query, callback, params) { //Array[Array] for rows of params, Array for params, value for one param, nothing for normal statement.
-		var res = [];
-		var db = new sqlite3.Database(file);
-		if(params && (params != undefined)) {
-			params = [].concat(params);
-			if(params.length==1) {
-				params[0] = [].concat(params[0]);
-			}
-		} else {
-			params=[[]];
-		}
-		
-		db.serialize(function() {
-			var stmt = db.prepare(query);
-			for(var i=0;i<params.length;i++) {
-				stmt.all.apply(stmt, params[i].concat(callback));
-			}
-			stmt.finalize();
-		});
-		db.close();
-		return res;
-	}
-}
->>>>>>> 75ec937c19886ec993b766c7e99c5af64da08826
