59c76887fec7208231baffcf3bcaaf44cfd5f5f8
Marcell Szakály <marci07iq@outlook.com>
2016-12-15 14:52:51 +0100

    New backend

--- db.js
+++ db.js
@@ -1,3 +1,37 @@
+<<<<<<< HEAD
+var fs = require("fs");
+var file = "test.db";
+var exists = fs.existsSync(file);
+var sqlite3 = require("sqlite3").verbose();
+
+module.exports = {
+	query: function(query, callback, params) { //Array[Array] for rows of params, Array for params, value for one param, nothing for normal statement.
+		//var res = [];
+		var db = new sqlite3.Database(file);
+		if(params && (params != undefined)) {
+			if(!Array.isArray(params)) {
+				params = [].concat(params);
+			}
+			if(params.length) {
+				params = [params];
+			} else {
+				params=[[]]
+			};
+		} else {
+			params=[[]];
+		}
+		console.log(params);
+		db.serialize(function() {
+			var stmt = db.prepare(query);
+			for(var i=0;i<params.length;i++) {
+				stmt.all.apply(stmt, params[i].concat(callback));
+			}
+			stmt.finalize();
+		});
+		db.close();
+	}
+}
+=======
 var fs = require("fs");
 var file = "test.db";
 var exists = fs.existsSync(file);
@@ -27,3 +61,4 @@ module.exports = {
 		return res;
 	}
 }
+>>>>>>> 75ec937c19886ec993b766c7e99c5af64da08826
