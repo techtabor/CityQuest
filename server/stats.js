a16c8b4bb394acbb27e0f0c6507e9af336e5d6fa
Marcell Szakály <marci07iq@outlook.com>
2017-05-03 19:55:55 +0200

    Added friend stat display

--- server/stats.js
+++ server/stats.js
@@ -68,18 +68,16 @@ module.exports = function() {
   this.GetFriendStats = function(req, res) {
     //console.log("asd");
     res.writeHead(200, base.head);
-    /*let params = JSON.parse(req.body);
+    let params = JSON.parse(req.body);
     getProfile(
       params.id_token, params.id_token_type,
       function(user) {
         base.maindb.query(
           //"SELECT Quests.Id, Quests.Name, Quests.Description, Questions.Count FROM Quests INNER JOIN Questions WHERE Quests.Id IN (SELECT Quests.Id FROM Quests INNERJOIN Solutions WHERE Solutions.User = ? AND Solutions.Quest = Quests.Id)"
-        "SELECT	Q.Id AS Id, S.Solved AS Solved, Q.Name AS Name, Q.Description AS Description, Qn.Total AS Questions FROM	Quests Q INNER JOIN ( (SELECT QuestId, COUNT(*) AS 'Total' FROM Questions GROUP BY QuestId) Qn INNER JOIN (SELECT Quest, COUNT(DISTINCT Question) AS 'Solved' FROM Solutions WHERE User = ? AND Question <> 0 GROUP BY Quest) S	ON Qn.QuestId = S.Quest) ON S.Quest = Q.Id",
+        "SELECT Users.Name AS 'Name', COUNT(DISTINCT Solutions.Question) AS 'Solved' FROM Solutions, TeamData, Users WHERE Question <> 0 AND Solutions.Team = TeamData.Id AND TeamData.Type = 1 AND TeamData.Leader = Users.Id AND Users.Id IN ( SELECT UserA FROM Friends WHERE UserB = ? AND Accepted = 1 ) GROUP BY Users.Name LIMIT 10",
           function(err, sqlres) {
             //console.log(JSON.stringify(sqlres));
-            sqlres.sort(function(a, b) {
-              return a.Solved / a.Total - b.Solved / b.Total;
-            });
+            //console.log(sqlres);
             res.write(JSON.stringify({Ok:0, Stats: sqlres}));
             res.end();
           },
@@ -95,8 +93,6 @@ module.exports = function() {
         res.write(JSON.stringify({Ok:1}));
         res.end();
       }
-    );*/
-    res.write(JSON.stringify({Ok:0, Stats: []}));
-    res.end();
+    );
   }
 }
